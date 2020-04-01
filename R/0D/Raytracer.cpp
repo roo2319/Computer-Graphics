f2f3c48e253d97186eabf7e85e202144139246d0
Ruairi Fox <rf17160@bris.ac.uk>
2020-04-01 17:35:23 +0100

    Parallel with omp raytracing

--- R/0D/Raytracer.cpp
+++ R/0D/Raytracer.cpp
@@ -22,6 +22,7 @@ bool closestIntersection(glm::vec3 start, glm::vec3 dir,
 void drawRaytraced(std::vector<ModelTriangle> model, DrawingWindow window, Camera camera){
   RayTriangleIntersection intersection;
   glm::vec3 dir;
+  #pragma omp parallel for private(intersection, dir)
   for(int y=0; y<window.height ;y++) {
     for(int x=0; x<window.width ;x++) {
       dir = glm::vec3(x-window.width/2,y-window.height/2,camera.focal);
